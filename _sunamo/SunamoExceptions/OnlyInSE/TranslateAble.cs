e2bec7385106f2517d2d91d3c114bb999412a9b5
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:45 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoExceptions/OnlyInSE/TranslateAble.cs
@@ -0,0 +1,17 @@
+
+namespace SunamoTextOutputGenerator;
+internal class TranslateAble
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    internal static string i18n(string xlfKey)
+    {
+        return xlfKey;
+    }
+}
