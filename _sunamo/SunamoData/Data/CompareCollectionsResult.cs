7abeea8197a0d477bc016d30a6b393a414b60d4f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 18:36:50 +0200

    All buildable, before removing dead code

--- _sunamo/SunamoData/Data/CompareCollectionsResult.cs
+++
@@ -1,9 +0,0 @@
-namespace SunamoTextOutputGenerator;
-
-
-public class CompareCollectionsResult<T>
-{
-    internal List<T> OnlyInFirst;
-    internal List<T> OnlyInSecond;
-    internal List<T> Both;
-}
